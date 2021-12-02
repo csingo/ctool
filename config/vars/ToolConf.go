fba7ebcc88f4623d5176405b2be049aa23c1fb11
chenxuyi <xuyi.chen@qdtech.ai>
2021-12-02 13:50:29 +0800

    update

--- config/vars/ToolConf.go
+++ config/vars/ToolConf.go
@@ -3,7 +3,7 @@ package vars
 import "gitee.com/csingo/ctool/config/typs"
 
 var Tool = &typs.ToolConf{
-	Version:               "v1.0.2",
+	Version:               "v1.0.4",
 	ProtoGenGoPackage:     "google.golang.org/protobuf/cmd/protoc-gen-go@v1.26.0",
 	ProtoGenGoGrpcPackage: "google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1.0",
 	ProtocDownload: &typs.ToolConf_ProtocAddr{
